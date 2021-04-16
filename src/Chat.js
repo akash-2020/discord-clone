4339c93bebf549f55cba956ccf023e53b3bd6027
akash-2020 <aakashbrahmbhatt31@gmail.com>
2021-04-16 17:15:11 +0530

    Changes in Chat section for overflow

--- src/Chat.js
+++ src/Chat.js
@@ -1,86 +1,92 @@
-import React, { useEffect, useState } from 'react';
-import  "./Chat.css";
-import ChatHeader from './ChatHeader';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
-import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
-import GifIcon from '@material-ui/icons/Gif';
-import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
-import Message from './Message';
-import { selectChannelId, selectUChannelName } from './features/appSlice';
-import { selectUser } from './features/userSlice';
-import { useSelector } from 'react-redux';
-import db from './firebase';
-import firebase from 'firebase';
+import React, { useEffect, useState } from "react";
+import "./Chat.css";
+import ChatHeader from "./ChatHeader";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
+import CardGiftcardIcon from "@material-ui/icons/CardGiftcard";
+import GifIcon from "@material-ui/icons/Gif";
+import EmojiEmotionsIcon from "@material-ui/icons/EmojiEmotions";
+import Message from "./Message";
+import { selectChannelId, selectUChannelName } from "./features/appSlice";
+import { selectUser } from "./features/userSlice";
+import { useSelector } from "react-redux";
+import db from "./firebase";
+import firebase from "firebase";
 
 function Chat() {
-    const user = useSelector(selectUser);
-     const channelId = useSelector(selectChannelId);
-     const channelName = useSelector(selectUChannelName);
-     const [input,setInput]=useState("");
-     const [messages,setMessages]=useState([]);
-     
-    useEffect(() => {
-        if(channelId){
-            db.collection("channels")
-            .doc(channelId)
-            .collection("messages")
-            .orderBy("timestamp","desc")
-            .onSnapshot((snapshot)=>
-                setMessages(snapshot.docs.map((doc)=>
-                    doc.data()
-                ))
-            );
-        }
-        
-    }, [channelId]);
+  const user = useSelector(selectUser);
+  const channelId = useSelector(selectChannelId);
+  const channelName = useSelector(selectUChannelName);
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
 
-    const sendMessage = (e) => {
-         e.preventDefault();
-        db.collection("channels").doc(channelId).collection("messages")
-        .add({
-            timestamp:firebase.firestore .FieldValue.serverTimestamp(),
-            message:input,
-            user:user
-        });
-        setInput("");
-     };   
-     
+  useEffect(() => {
+    if (channelId) {
+      db.collection("channels")
+        .doc(channelId)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+    }
+  }, [channelId]);
 
-    return (
-        <div className="chat">
-            <ChatHeader channelName={channelName}/>
-            <div className="chat__messages">
-                {
-                    messages.map((message)=>(
-                        <Message 
-                            timestamp={message.timestamp}
-                            message={message.message}
-                            user={message.user}
-                        /> 
-                    )
-                    )
-                }
-            </div>
-            <div className="chat__input">
-                <AddCircleIcon fontSize="large"/>
-                <form>
-                    <input value={input} 
-                     disabled={!channelId}
-                     onChange={(e)=> setInput(e.target.value)}
-                     placeholder={`Message #${channelName}`}/>
-                    <button className="chat__inputButton" 
-                    type="submit"
-                    onClick={sendMessage}>Send Message</button>
-                </form>
+  const sendMessage = (e) => {
+    var x = document.forms["chatForm"]["input"].value;
+    if (x == "") {
+      alert("No message to send!!");
+      return false;
+    } else {
+      e.preventDefault();
+      db.collection("channels").doc(channelId).collection("messages").add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        message: input,
+        user: user,
+      });
+      setInput("");
+    }
+  };
 
-                <div className="chat__inputIcons">
-                    <CardGiftcardIcon />
-                    <GifIcon />
-                    <EmojiEmotionsIcon />
-                </div>
-            </div>
+  return (
+    <div className="chat">
+      <ChatHeader channelName={channelName} />
+      <div className="chat__messages">
+        {messages.map((message) => (
+          <Message
+            timestamp={message.timestamp}
+            message={message.message}
+            user={message.user}
+          />
+        ))}
+      </div>
+      <div className="chat__input">
+        <AddCircleIcon fontSize="large" />
+        <form name="chatForm">
+          <input
+            name="input"
+            value={input}
+            required
+            disabled={!channelId}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder={`Message #${channelName}`}
+          />
+          <button
+            className="chat__inputButton"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send Message
+          </button>
+        </form>
+
+        <div className="chat__inputIcons">
+          <CardGiftcardIcon />
+          <GifIcon />
+          <EmojiEmotionsIcon />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
